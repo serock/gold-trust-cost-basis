a6e44270ee913e3fc524738d3e2c6c58fbbda839
John Serock <1468970+serock@users.noreply.github.com>
2025-03-29 18:37:42 -0400

    Last phase of rearchitecting the app

--- src/main/java/spreadsheet/sheet/tax/GoldSalesSheetBuilder.java
+++ src/main/java/spreadsheet/sheet/tax/GoldSalesSheetBuilder.java
@@ -16,12 +16,9 @@ import com.sun.star.sheet.XSpreadsheet;
 import spreadsheet.SpreadsheetDocumentHelper;
 import spreadsheet.sheet.SheetBuilder;
 import spreadsheet.sheet.SheetHelper;
+import text.Constants;
 
 public class GoldSalesSheetBuilder extends SheetBuilder {
-
-    private static int GGP_DATE_INDEX = 0;
-    private static int TL_DATE_ACQUIRED_INDEX = 2;
-    private static int TL_DATE_SOLD_INDEX = 3;
 
     @Override
     public void build() throws com.sun.star.uno.Exception {
@@ -55,20 +52,6 @@ public class GoldSalesSheetBuilder extends SheetBuilder {
         columnPropertiesCollection.add(columnProperties);
     }
 
-    private void addGoldOuncesOwnedColumnProperties(final List<SortedMap<String, Object>> columnPropertiesCollection) {
-        final int ouncesFormatIndexKey = SpreadsheetDocumentHelper.queryNumberFormatCode(document(), "#,##0.00000000;[RED]-#,##0.00000000");
-        final SortedMap<String, Object> columnProperties = new TreeMap<>();
-        addNumberFormatColumnProperty(columnProperties, Integer.valueOf(ouncesFormatIndexKey));
-        columnPropertiesCollection.add(columnProperties);
-    }
-
-    private void addGoldOuncesSoldColumnProperties(final List<SortedMap<String, Object>> columnPropertiesCollection) {
-        final int ouncesFormatIndexKey = SpreadsheetDocumentHelper.queryNumberFormatCode(document(), "#,##0.00000000;[RED]-#,##0.00000000");
-        final SortedMap<String, Object> columnProperties = new TreeMap<>();
-        addNumberFormatColumnProperty(columnProperties, Integer.valueOf(ouncesFormatIndexKey));
-        columnPropertiesCollection.add(columnProperties);
-    }
-
     private static void addNumberFormatColumnProperty(final SortedMap<String, Object> columnProperties, final Integer indexKey) {
         columnProperties.put("NumberFormat", indexKey);
     }
@@ -77,6 +60,11 @@ public class GoldSalesSheetBuilder extends SheetBuilder {
         final SortedMap<String, Object> columnProperties = new TreeMap<>();
         columnProperties.put("NumberFormat", SpreadsheetDocumentHelper.getTextFormat(document()));
         columnPropertiesCollection.add(columnProperties);
+    }
+
+    private Object[][] costBasisFactorData() throws WrappedTargetException, NoSuchElementException {
+        final XSpreadsheet costBasisFactorsSheet = SpreadsheetDocumentHelper.getSheet(document(), "cost-basis-factors");
+        return SheetHelper.getData(costBasisFactorsSheet);
     }
 
     private SortedMap<String, Object> createHeaderProperties() {
@@ -90,8 +78,6 @@ public class GoldSalesSheetBuilder extends SheetBuilder {
         final List<SortedMap<String, Object>> columnPropertiesCollection = new ArrayList<>(6);
         addTaxLotIdColumnProperties(columnPropertiesCollection);
         addDateColumnProperties(columnPropertiesCollection);
-        addGoldOuncesSoldColumnProperties(columnPropertiesCollection);
-        addGoldOuncesOwnedColumnProperties(columnPropertiesCollection);
         addCostOfGoldSoldColumnProperties(columnPropertiesCollection);
         addAdjustedCostBasisColumnProperties(columnPropertiesCollection);
         return columnPropertiesCollection;
@@ -101,25 +87,24 @@ public class GoldSalesSheetBuilder extends SheetBuilder {
         List<String[]> rowsCollection = new ArrayList<>();
         rowsCollection.add(headerRow());
         final int taxYear = taxYear();
-        final Object[][] goldOuncesData = goldOuncesData();
-        final Object[][] grossProceedsData = grossProceedsData();
+        final Object[][] costBasisFactorData = costBasisFactorData();
         final Object[][] taxLotData = taxLotData();
         int numberOfTaxLots = taxLotData.length - 1;
         for (int i = 1; i <= numberOfTaxLots; i++) {
             final Object[] row = taxLotData[i];
-            final Double dateAcquiredObject = ((Double) row[TL_DATE_ACQUIRED_INDEX]); 
+            final Double dateAcquiredObject = ((Double) row[Constants.TL_FIELD_DATE_ACQUIRED]);
             final double dateAcquired = dateAcquiredObject.doubleValue();
             final int dateAcquiredYear = SpreadsheetDocumentHelper.year(dateAcquired);
             final Double dateSoldObject =
-                    (row[TL_DATE_SOLD_INDEX] instanceof Double) ?
-                            ((Double) row[TL_DATE_SOLD_INDEX]) :
+                    (row[Constants.TL_FIELD_DATE_SOLD] instanceof Double) ?
+                            ((Double) row[Constants.TL_FIELD_DATE_SOLD]) :
                                 SpreadsheetDocumentHelper.dateValue(Integer.toString(taxYear + 1) + "-01-01");
             final double dateSold = dateSoldObject.doubleValue();
             if (dateAcquiredYear > taxYear) {
                 continue;
             } else if (dateAcquiredYear < taxYear) {
-                if (Arrays.stream(grossProceedsData).skip(1).anyMatch(t -> ((Double) t[GGP_DATE_INDEX]).doubleValue() < dateSold)) {
-                    rowsCollection.add(new String[] {"=$'tax-lots'.A" + Integer.toString(i + 1), "", "", "", "", ""});
+                if (Arrays.stream(costBasisFactorData).skip(1).anyMatch(t -> ((Double) t[Constants.CBF_FIELD_DATE]).doubleValue() < dateSold)) {
+                    rowsCollection.add(new String[] {"=$'tax-lots'.A" + Integer.toString(i + 1), "", "", ""});
                 }
             } else {
                 rowsCollection.add(
@@ -127,45 +112,29 @@ public class GoldSalesSheetBuilder extends SheetBuilder {
                                 "=$'tax-lots'.A" + Integer.toString(i + 1),
                                 "",
                                 "",
-                                "=$'tax-lots'.B" + Integer.toString(i + 1) + "*VLOOKUP($'tax-lots'.C" + Integer.toString(i + 1) +";$'gld-gold-ounces'.A1:B" + Integer.toString(goldOuncesData.length) + ";2)",
-                                "",
                                 "=$'tax-lots'.F" + Integer.toString(i + 1)
                                 });
             }
-            final int[] saleIndices = IntStream.range(1, grossProceedsData.length)
-                .filter(j -> ((Double) grossProceedsData[j][GGP_DATE_INDEX]).compareTo(dateAcquiredObject) > 0 && ((Double) grossProceedsData[j][GGP_DATE_INDEX]).compareTo(dateSoldObject) < 0)
+            final int[] factorIndices = IntStream.range(1, costBasisFactorData.length)
+                .filter(j -> ((Double) costBasisFactorData[j][Constants.CBF_FIELD_DATE]).compareTo(dateAcquiredObject) > 0 && ((Double) costBasisFactorData[j][Constants.CBF_FIELD_DATE]).compareTo(dateSoldObject) < 0)
                 .toArray();
-            for (int saleIndex : saleIndices) {
+            for (int factorIndex : factorIndices) {
                 rowsCollection.add(
                         new String[] {
                                 "=$'tax-lots'.A" + Integer.toString(i + 1),
-                                "=$'gld-gross-proceeds'.A" + Integer.toString(saleIndex + 1),
-                                "=$'tax-lots'.B" + Integer.toString(i + 1) + "*$'gld-gross-proceeds'.B" + Integer.toString(saleIndex + 1),
-                                "=D" + Integer.toString(rowsCollection.size()) + "-C" + Integer.toString(rowsCollection.size() + 1),
-                                "=ROUND(F" + Integer.toString(rowsCollection.size()) + "*C" + Integer.toString(rowsCollection.size() + 1) + "/D" + Integer.toString(rowsCollection.size()) + ";2)",
-                                "=F" + Integer.toString(rowsCollection.size()) + "-E" + Integer.toString(rowsCollection.size() + 1)
+                                "=$'cost-basis-factors'.A" + Integer.toString(factorIndex + 1),
+                                "=$'cost-basis-factors'.B" + Integer.toString(factorIndex + 1) + "*D" + Integer.toString(rowsCollection.size()),
+                                "=$D" + Integer.toString(rowsCollection.size()) + "-C" + Integer.toString(rowsCollection.size() + 1)
                                 });
             }
         }
         return rowsCollection.toArray(new String[0][0]);
     }
 
-    private Object[][] goldOuncesData() throws WrappedTargetException, NoSuchElementException {
-        final XSpreadsheet gldGoldOuncesSheet = SpreadsheetDocumentHelper.getSheet(document(), "gld-gold-ounces");
-        return SheetHelper.getData(gldGoldOuncesSheet);
-    }
-
-    private Object[][] grossProceedsData() throws WrappedTargetException, NoSuchElementException {
-        final XSpreadsheet gldGrossProceedsSheet = SpreadsheetDocumentHelper.getSheet(document(), "gld-gross-proceeds");
-        return SheetHelper.getData(gldGrossProceedsSheet);
-    }
-
     private static String[] headerRow() {
         return new String[] {
                 "Tax Lot ID",
                 "Date of Gold Sale",
-                "Gold Ounces Sold",
-                "Gold Ounces Owned",
                 "Cost of Gold Sold",
                 "Adjusted Cost Basis"
         };
@@ -177,8 +146,8 @@ public class GoldSalesSheetBuilder extends SheetBuilder {
     }
 
     private int taxYear() throws com.sun.star.uno.Exception {
-        final XSpreadsheet gldGoldOuncesSheet = SpreadsheetDocumentHelper.getSheet(document(), "gld-gold-ounces");
-        final double date = SheetHelper.getCellValue(gldGoldOuncesSheet, "A2");
+        final XSpreadsheet costBasisFactorsSheet = SpreadsheetDocumentHelper.getSheet(document(), "cost-basis-factors");
+        final double date = SheetHelper.getCellValue(costBasisFactorsSheet, "A2");
         return SpreadsheetDocumentHelper.year(date);
     }
 }
--- src/main/java/text/Constants.java
+++ src/main/java/text/Constants.java
@@ -7,8 +7,9 @@ public class Constants {
 
     public static final int GS_FIELD_TAX_LOT_ID = 0;
     public static final int GS_FIELD_DATE_OF_GOLD_SALE = 1;
-    public static final int GS_FIELD_GOLD_OUNCES_SOLD = 2;
-    public static final int GS_FIELD_GOLD_OUNCES_OWNED = 3;
-    public static final int GS_FIELD_COST_OF_GOLD_SOLD = 4;
-    public static final int GS_FIELD_ADJUSTED_COST_BASIS = 5;
+    public static final int GS_FIELD_COST_OF_GOLD_SOLD = 2;
+    public static final int GS_FIELD_ADJUSTED_COST_BASIS = 3;
+
+    public static final int TL_FIELD_DATE_ACQUIRED = 2;
+    public static final int TL_FIELD_DATE_SOLD = 3;
 }
